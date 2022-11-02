fc8184b7b8d92969eb1715acec3a8bde93bd4c16
KaigaMunene <kaigamunene@gmail.com>
2022-11-02 04:08:55 +0300

    console.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,31 +1,41 @@
 #!/usr/bin/python3
 """
-    This contains the FileStorage module
+This module contains the FileStorage class.
 """
 import json
+import os
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
-valid_classes = {"BaseModel": BaseModel,
-                 "User": User}
+valid_classes = {'BaseModel': BaseModel,
+                 'User': User,
+                 'State': State,
+                 'City': City,
+                 'Amenity': Amenity,
+                 'Place': Place,
+                 'Review': Review}
 
 
 class FileStorage():
-    """
-        Comment
-    """
+    """Class used for file storage actions."""
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """A method that returns the dictionary __objects."""
-        return self.__objects
+        return(self.__objects)
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        obj_key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[obj_key] = obj
 
     def save(self):
         """A method that serializes __objects to the JSON file."""
